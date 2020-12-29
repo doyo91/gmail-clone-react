117a3cb7a400e4066a6d8d4679cc21cc1718b95c
doyo91 <samuel.gil91dev@gmail.com>
2020-12-29 12:01:31 +0100

    header added

--- src/components/shared/Header/index.js
+++ src/components/shared/Header/index.js
@@ -1,7 +1,11 @@
 import React from "react"
 import styles from "./Header.module.css"
-import { IconButton } from "@material-ui/core"
-import MenuIcon from "@material-ui/icons"
+import { Avatar, IconButton } from "@material-ui/core"
+import MenuIcon from "@material-ui/icons/Menu"
+import SearchIcon from "@material-ui/icons/Search"
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown"
+import AppsIcon from "@material-ui/icons/Apps"
+import NotificationsIcon from "@material-ui/icons/Notifications"
 
 export const Header = () => {
   return (
@@ -10,9 +14,34 @@ export const Header = () => {
         <IconButton>
           <MenuIcon />
         </IconButton>
+        <img
+          className={styles.logo}
+          src="https://i.pinimg.com/originals/ae/47/fa/ae47fa9a8fd263aa364018517020552d.png"
+        />
       </div>
-      <div className={styles.center}></div>
-      <div className={styles.right}></div>
+      <div className={styles.middle}>
+        <IconButton>
+          <SearchIcon />
+        </IconButton>
+
+        <input
+          className={styles.input}
+          placeholder="Buscar correo"
+          type="text"
+        />
+        <IconButton className={styles.iconButton}>
+          <ArrowDropDownIcon className={styles.iconInput} />
+        </IconButton>
+      </div>
+      <div className={styles.right}>
+        <IconButton>
+          <AppsIcon />
+        </IconButton>
+        <IconButton>
+          <NotificationsIcon />
+        </IconButton>
+        <Avatar />
+      </div>
     </header>
   )
 }
