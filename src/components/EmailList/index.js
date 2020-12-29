530d668c89f91813eb907b4270facbd8099f69e1
doyo91 <samuel.gil91dev@gmail.com>
2020-12-29 13:30:43 +0100

    settings email list added

--- src/components/EmailList/index.js
+++ src/components/EmailList/index.js
@@ -5,6 +5,10 @@ import { Checkbox, IconButton } from "@material-ui/core"
 import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown"
 import RedoIcon from "@material-ui/icons/Redo"
 import MoreVertIcon from "@material-ui/icons/MoreVert"
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft"
+import ChevronRightIcon from "@material-ui/icons/ChevronRight"
+import KeyboardHideIcon from "@material-ui/icons/KeyboardHide"
+import SettingsIcon from "@material-ui/icons/Settings"
 
 export const EmailList = () => {
   return (
@@ -22,7 +26,20 @@ export const EmailList = () => {
             <MoreVertIcon />
           </IconButton>
         </div>
-        <div className={styles.settingsRight}></div>
+        <div className={styles.settingsRight}>
+          <IconButton>
+            <ChevronLeftIcon />
+          </IconButton>
+          <IconButton>
+            <ChevronRightIcon />
+          </IconButton>
+          <IconButton>
+            <KeyboardHideIcon />
+          </IconButton>
+          <IconButton>
+            <SettingsIcon />
+          </IconButton>
+        </div>
       </div>
     </div>
   )
